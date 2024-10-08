85b5d552c8475a3f18952c8b511f0f104331ee94
Zach Lewis <zachcanbereached@gmail.com>
2024-10-08 09:27:27 -0400

    deps(PNG): use find_package in build recipe
    Signed-off-by: Zach Lewis <zachcanbereached@gmail.com>

--- src/cmake/build_PNG.cmake
+++ src/cmake/build_PNG.cmake
@@ -29,13 +29,16 @@ build_dependency_with_cmake(PNG
         -D CMAKE_INSTALL_LIBDIR=lib
     )
 
-set (PNG_ROOT ${PNG_LOCAL_INSTALL_DIR})
-set (PNG_DIR ${PNG_LOCAL_INSTALL_DIR}/lib/cmake/PNG)
 
-# # Signal to caller that we need to find again at the installed location
-set (PNG_REFIND TRUE)
-set (PNG_REFIND_VERSION ${PNG_BUILD_VERSION})
-set (PNG_REFIND_ARGS NO_DEFAULT_PATH)
+if (PNG_BUILD_VERSION VERSION_LESS 1.6.44)
+    set (PNG_DIR ${PNG_LOCAL_INSTALL_DIR}/lib/libpng/libpng16)
+else ()
+    set (PNG_DIR ${PNG_LOCAL_INSTALL_DIR}/lib/cmake/PNG)
+endif ()
+
+find_package(PNG ${PNG_BUILD_VERSION} REQUIRED
+             PATHS "${PNG_LOCAL_INSTALL_DIR}" "${PNG_DIR}"
+             NO_DEFAULT_PATH)
 
 if (PNG_BUILD_SHARED_LIBS)
     install_local_dependency_libs (PNG png16)
